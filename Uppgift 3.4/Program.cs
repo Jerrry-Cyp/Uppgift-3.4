33c93ad5752106179f65d65225ace779cc497241
alsa07007 <alsa07007@E-PW05CGR5>
2024-11-13 12:31:19 +0100

    Add project files.

---
+++ Uppgift 3.4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift_3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ange låt tid: (Minuter)");
+            int Min = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ange låt tid: (Sekunder)");
+            int Sek = int.Parse(Console.ReadLine());
+
+            if (Min >= 2 && Min <= 4)
+            {
+                if (Min >= 2 && Sek >= 45)
+                {
+                    Console.WriteLine("Du får spela din låt på radion");
+                }
+                if (Min <= 4 && Sek <= 20)
+                {
+                    Console.WriteLine("Du får spela din låt på radion");
+                }
+            }
+            else Console.WriteLine("Du får inte spela din låt på radiostationen");
+        }
+    }
+}
